323356377b5d0a6b767e99ae117692dafd351b68
athenamw <wallisathena@gmail.com>
2023-09-20 20:51:20 -0700

    created get function in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,6 +25,17 @@ export const putDb = async (content) => {
   // console.error("putDb not implemented");
 };
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  console.log("GET database items");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+  // error no longer needed now that putDb implemented.
+  // console.error("putDb not implemented");
+};
 
 initdb();
